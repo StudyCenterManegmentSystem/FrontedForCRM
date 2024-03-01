11043feeabaec57ea08cc468c02765d7bc975d20
Asilkhoja Mansurov <asilxojamansurov217@gmail.com>
2024-03-01 17:23:17 +0500

    Teacher boldi

---
+++ template/js/Course/addcourse.js
@@ -0,0 +1,38 @@
+async function addCourse() {
+    const name = document.getElementById("name").value;
+    const description = document.getElementById("description").value;
+    const courseData = {
+        fanName: name,
+        fanDescription: description
+    }
+
+    try{
+        const response = await fetch('https://localhost:44334/api/fans/create-fan', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(courseData)
+        });
+
+        if (response.ok === true || response.status === 201 || response.status === 200) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = "Kurs muvaffaqiyatli qo'shildi.";
+            resultDiv.style.color = 'green';
+            resultDiv.style.display = 'block';
+
+            document.getElementById('errorDisplay').innerText = '';
+            setTimeout(() => {
+                window.location.href = './all-courses.html';
+            }, 2000);
+        } else if (response.status === 400 || response.status === 404 || response.status === 500) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = "Kurs qo'shib bo'lmadi. Iltimos, qayta urunib ko'ring.";
+            resultDiv.style.color = 'red';
+            resultDiv.style.display = 'block';
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
---
+++ template/js/Professer/delete-professor.js
@@ -0,0 +1,42 @@
+async function deleteProfessor() {
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+
+    const professorData = {
+        password: name,
+        email: email
+    }
+
+    try {
+        const response = await fetch('https://localhost:44334/api/admins/delete-teacher-account', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(professorData)
+        });
+
+        if (response.ok || response.status === 200 || response.status === 201) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = "Ustoz muvaffaqiyatli o'chirildi";
+            resultDiv.style.color = 'green';
+            resultDiv.style.display = 'block';
+
+            document.getElementById('errorDisplay').innerText = '';
+             setTimeout(() => {
+                window.location.href = './all-professors.html';
+            }, 2000);
+        }
+        else if (response.status === 400 || response.status === 404 || response.status === 500) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = "Parol yoki email noto'g'ri.";
+            resultDiv.style.color = 'red';
+            resultDiv.style.display = 'block';
+
+            document.getElementById('errorDisplay').innerText = ''
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
