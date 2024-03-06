7d90dbeef82f7eeba50eff60696c64da950c1400
Intel Computers <Intel Computers@DESKTOP-EV5JP40>
2024-03-06 08:36:48 +0300

    Updated

---
+++ template/js/Course/allcourse.js
@@ -0,0 +1,44 @@
+const apiUrl = 'https://localhost:7177/api/fans/get-all-with-teachers';
+
+async function fetchCourses() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching courses:', error);
+        throw error;
+    }
+}
+
+function renderCourses(courses) {
+    const courseTableBody = document.getElementById('courseTableBody');
+
+    courseTableBody.innerHTML = '';
+
+    courses.forEach(course => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${course.fanName}</td>
+            <td>${getTeacherNames(course.teachers)}</td>
+        `;
+        courseTableBody.appendChild(row);
+    });
+}
+
+
+function getTeacherNames(teachers) {
+    return teachers.map(teacher => `${teacher.firstName} ${teacher.lastName}`).join(', ');
+}
+
+
+async function init() {
+    try {
+        const courses = await fetchCourses();
+
+        renderCourses(courses);
+    } catch (error) {
+        console.error('Error initializing application:', error);
+    }
+}
+init();
