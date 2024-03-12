d22d6603f94b62b6315e275300cabfc021a702e6
Asilkhoja Mansurov <asilxojamansurov217@gmail.com>
2024-03-12 15:18:33 +0500

    bug fix ed

--- template/js/Group/all-group.js
+++ template/js/Group/all-group.js
@@ -15,33 +15,34 @@ function fetchGroupData() {
     });
 }
 
-// Guruhni ID orqali o'chirish funktsiyasi
-// Guruhni ID orqali o'chirish funktsiyasi
+// Delete group function
 function deleteGroup(groupId) {
-    // AJAX so'rovini yuborish
+    // Make AJAX request
     $.ajax({
         url: `https://localhost:7177/api/groups/delete-guruh/${groupId}`,
         type: 'DELETE',
         dataType: 'json',
         success: function (data) {
-            // HTML dan o'chirilgan guruhni o'chirish (tanlangan)
+            // Reload the page after successful deletion
             location.reload();
-            console.log('Guruh muvaffaqiyatli o\'chirildi');
+            console.log('Group deleted successfully');
         },
         error: function (xhr, status, error) {
+            // Reload the page if there's an error
             location.reload();
-            console.error('Xatolik guruhni o\'chirishda:', error);
+            console.error('Error deleting group:', error);
         }
     });
 }
 
-// Click hodisasini qo'llash uchun tanlangan element
+// Click event handler for delete button
 $('.row').on('click', '.delete-btn', function (e) {
     e.preventDefault();
     var groupId = $(this).data('id');
     // Call deleteGroup function
     deleteGroup(groupId);
 });
+
 // Function to populate HTML with group data
 function populateGroupHTML(data) {
     // Get the container element where group cards will be appended
@@ -57,35 +58,52 @@ function populateGroupHTML(data) {
 
         // Create HTML for group card
         var groupHTML = `
-                <div class='col-xl-3 col-xxl-4 col-lg-4 col-md-6 col-sm-6 mt-3 ml-2'>
-                    <div class='card'>
-                        <!-- Populate card content with group data -->
-                        <div class='card-body'>
-                            <h4>${group.fan.fanName}</h4>
-                            <ul class='list-group mb-3 list-group-flush'>
-                                <li class='list-group-item px-0 border-top-0 d-flex justify-content-between'>
-                                    <span class='mb-0 text-muted'>${formattedStart}</span>
-                                </li>
-                                <li class='list-group-item px-0 d-flex justify-content-between'>
-                                    <span class='mb-0'>Duration :</span>
-                                    <strong>${group.duration}</strong>
-                                </li>
-                                <li class='list-group-item px-0 d-flex justify-content-between'>
-                                    <span class='mb-0'>Professor :</span>
-                                    <strong>${group.teacher.firstName} ${group.teacher.lastName}</strong>
-                                </li>
-                                <!-- other list items... -->
-                                <li class='list-group-item px-0 d-flex justify-content-between'>
+            <div class='col-xl-3 col-xxl-4 col-lg-4 col-md-6 col-sm-6 mt-3 ml-2'>
+                <div class='card'>
+                    <!-- Populate card content with group data -->
+                    <div class='card-body'>
+                        <h4>${group.fan.fanName}</h4>
+                        <ul class='list-group mb-3 list-group-flush'>
+                            <li class='list-group-item px-0 border-top-0 d-flex justify-content-between'>
+                                <span class='mb-0 text-muted'>${formattedStart}</span>
+                            </li>
+                            <li class='list-group-item px-0 d-flex justify-content-between'>
+                                <span class='mb-0'>Duration :</span>
+                                <strong>${group.duration}</strong>
+                            </li>
+                            <li class='list-group-item px-0 d-flex justify-content-between'>
+                                <span class='mb-0'>Professor :</span>
+                                <strong>${group.teacher.firstName} ${group.teacher.lastName}</strong>
+                            </li>
+                           <li class='list-group-item px-0 d-flex justify-content-between'>
+                                <span>
+                                    <i class='text-primary mr-2'></i>Start
+                                </span>
+                                <strong>${formatDateTime(group.start)}</strong>
+                            </li>
+                            <li class='list-group-item px-0 d-flex justify-content-between'>
+                                <span>
+                                    <i class='text-primary mr-2'></i>End
+                                </span>
+                                <strong>${formatDateTime(group.end)}</strong>
+                            </li>
+                            <li class='list-group-item px-0 d-flex justify-content-between'>
+                                <span>
+                                    <i class='text-primary mr-2'></i>Room 
+                                </span>
+                                <strong>${group.room.roomName}</strong>
+                            </li>
+                            <li class='list-group-item px-0 d-flex justify-content-between'>
                                 <span>
                                     <i class="text-primary mr-2"></i>Delete Group
                                 </span>
                                 <strong><a href="all-group.html" class="delete-btn btn btn-danger" data-id="${group.id}">Delete</a></strong>
-                                </li>
-                            </ul>
-                        </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            `;
+            </div>
+        `;
         container.append(groupHTML);
     });
 }
@@ -103,8 +121,13 @@ function formatDateTime(datetimeString) {
     var month = monthNames[date.getMonth()];
     var day = date.getDate();
     var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
 
-    var formattedDate = month + ' ' + (day < 10 ? '0' + day : day) + ', ' + year;
+    // Formatting hours and minutes with leading zeros if necessary
+    var formattedTime = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+
+    var formattedDate = month + ' ' + (day < 10 ? '0' + day : day) + ', ' + year + ' ' + formattedTime;
 
     return formattedDate;
 }
