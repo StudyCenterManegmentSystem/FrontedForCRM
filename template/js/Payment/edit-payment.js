a2c1e96359443bbf6f5e075fc39a7a650f335ca8
Intel Computers <Intel Computers@DESKTOP-EV5JP40>
2024-03-27 08:11:09 +0300

    Edit payment, Full Payment tugadi

---
+++ template/js/Payment/edit-payment.js
@@ -0,0 +1,34 @@
+function editPayment() {
+    const id = document.getElementById('id').value;
+    const studentId = document.getElementById('talabaId').value;
+    const groupId = document.getElementById('groupId').value;
+    const qachonTolagan = document.getElementById('qachonTolangan').value;
+    const qanchaTolagan = parseInt(document.getElementById('qanchaTolangan').value);
+    const paymentType = parseInt(document.getElementById('paymentType').value);
+    const data = {
+        id: id,
+        studentId: studentId,
+        groupId: groupId,
+        qachonTolagan: qachonTolagan,
+        qanchaTolagan: qanchaTolagan,
+        paymentType: paymentType
+    };
+    fetch('https://localhost:7177/api/payments/update-payment', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById('result').textContent = 'Payment updated successfully.';
+        } else {
+            document.getElementById('errorDisplay').textContent = 'Failed to update payment. Please try again.';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('errorDisplay').textContent = 'An error occurred. Please try again later.';
+    });
+}
